ca417a2acdb1ca7fc736c59b89a5318fe1764d8f
Алексей Фирсов <adfirso2@mts.ru>
2022-05-12 11:17:58 +0300

    Initial Commit

---
+++ EasyPoker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyPoker
+//
+//  Created by Фирсов Алексей on 12.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
